924bee83a65e65ec07191ea4bd8eb246916db01a
Montse Lozano Dieck <malozano@microsoft.com>
2018-01-31 10:20:26 -0800

    Add styling changes to keytip

--- packages/office-ui-fabric-react/src/components/Keytip/Keytip.styles.ts
+++ packages/office-ui-fabric-react/src/components/Keytip/Keytip.styles.ts
@@ -1,9 +1,36 @@
+import { IKeytipStyleProps, IKeytipStyles } from './Keytip.types';
 import { ICalloutContentStyleProps, ICalloutContentStyles } from '../Callout/Callout.types';
 
+export const getStyles = (props: IKeytipStyleProps): IKeytipStyles => {
+  const { theme, disabled } = props;
+  return {
+    container: [
+      {
+        backgroundColor: theme.palette.neutralDark
+      },
+      disabled && {
+
+      }
+    ],
+    root: [{
+      textAlign: 'center',
+      paddingLeft: 3,
+      paddingRight: 3,
+      backgroundColor: theme.palette.neutralDark,
+      color: theme.palette.neutralLight,
+      minWidth: 11,
+      lineHeight: 17,
+      height: 17,
+      display: 'inline-block'
+    },
+    disabled && {
+
+    }],
+  };
+};
+
 export const getCalloutStyles = (props: ICalloutContentStyleProps): ICalloutContentStyles => {
-
   const { theme } = props;
-
   return {
     container: [
     ],
--- packages/office-ui-fabric-react/src/components/Keytip/Keytip.types.ts
+++ packages/office-ui-fabric-react/src/components/Keytip/Keytip.types.ts
@@ -1,14 +1,19 @@
 import * as React from 'react';
 import { Keytip } from '../../Keytip';
 import { ICalloutProps } from '../../Callout';
-import { IPoint, IKeySequence } from '../../Utilities';
+import { IStyle, ITheme } from '../../Styling';
+import { IPoint, IKeySequence, IStyleFunction } from '../../Utilities';
 
-export interface IKeytipProps extends React.Props<Keytip> {
+export interface IKeytip {
+
+}
+
+export interface IKeytipProps {
   /**
    * Optional callback to access the Keytip component. Use this instead of ref for accessing
    * the public methods and properties of the component.
    */
-  componentRef?: (component: Keytip) => void;
+  componentRef?: (component: IKeytip) => void;
 
   /**
    * The DOM ID to use for the keytip HTML element
@@ -25,6 +30,11 @@ export interface IKeytipProps extends React.Props<Keytip> {
    * @memberof IKeytipProps
    */
   content?: string;
+
+  /**
+  * Optional theme for component
+  */
+  theme?: ITheme;
 
   /**
    * T/F if the corresponding control is disabled
@@ -57,9 +67,24 @@ export interface IKeytipProps extends React.Props<Keytip> {
   calloutProps?: ICalloutProps;
 
   /**
+  * Optional styles for the component.
+  */
+  getStyles?: IStyleFunction<IKeytipStyleProps, IKeytipStyles>;
+
+  /**
    * The target that the Keytip should try to position itself based on.
    * It can be either an HTMLElement a querySelector string of a valid HTMLElement
    * or a MouseEvent. If MouseEvent is given then the origin point of the event will be used.
    */
   keytipTarget?: HTMLElement | string | MouseEvent | IPoint | null;
+}
+
+export interface IKeytipStyleProps {
+  theme: ITheme;
+  disabled?: boolean;
+}
+
+export interface IKeytipStyles {
+  container: IStyle;
+  root: IStyle;
 }
