b6acbc41501f8e875b9644e3039dd8497a131299
montselozanod <montse_967@hotmail.com>
2018-01-31 08:40:36 -0800

    Add sequence func

--- packages/utilities/src/KeySequence.ts
+++ packages/utilities/src/KeySequence.ts
@@ -3,3 +3,11 @@ import { KeyCodes } from './KeyCodes';
 export interface IKeySequence {
   keyCodes: KeyCodes[];
 }
+
+export function convertSequencesToString(keySequences: IKeySequence[]): string {
+  let conversion = '';
+  for (let keySequence of keySequences) {
+    conversion+= keySequence.keyCodes.join('-');
+  }
+  return conversion;
+}
