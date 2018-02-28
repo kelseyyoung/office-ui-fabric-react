eff68097c3841b6ec1e5021fd4a078a88376727e
Kelsey Young <keyou@microsoft.com>
2018-02-28 11:05:35 -0800

    Update KeytipConfig to save entire IKeytipProps

--- packages/experiments/src/utilities/keytip/KeytipConfig.test.ts
+++ packages/experiments/src/utilities/keytip/KeytipConfig.test.ts
@@ -8,35 +8,52 @@ describe('KeytipConfig', () => {
         {
           id: 'keytip1',
           sequence: { keys: ['a'] },
+          content: 'A',
+          optionalProps: {
+            onExecute: jest.fn()
+          },
           children: [
             {
               id: 'keytip2',
               sequence: { keys: ['b', '1'] },
+              content: 'B1',
+              optionalProps: {
+                disabled: true
+              },
               children: [
                 {
                   id: 'keytip3',
+                  content: 'C',
                   sequence: { keys: ['c'] }
                 }
               ]
             },
             {
               id: 'keytip4',
-              sequence: { keys: ['t', '2'] }
+              sequence: { keys: ['t', '2'] },
+              content: 'T2'
             },
             {
               id: 'keytip5',
               sequence: { keys: ['f'] },
+              content: 'F',
               children: [
                 {
                   id: 'keytip6',
-                  sequence: { keys: ['x', '0'] }
+                  sequence: { keys: ['x', '0'] },
+                  content: 'X0',
+                  optionalProps: {
+                    onReturn: jest.fn()
+                  },
                 },
                 {
                   id: 'keytip7',
                   sequence: { keys: ['y', 'y'] },
+                  content: 'YY',
                   children: [
                     {
                       id: 'keytip8',
+                      content: 'R',
                       sequence: { keys: ['r'] }
                     },
                   ]
@@ -59,13 +76,47 @@ describe('KeytipConfig', () => {
     let keytip7Seq: IKeySequence[] = [{ keys: ['a'] }, { keys: ['f'] }, { keys: ['y', 'y'] }];
     let keytip8Seq: IKeySequence[] = [{ keys: ['a'] }, { keys: ['f'] }, { keys: ['y', 'y'] }, { keys: ['r'] }];
 
-    expect(fullKeySequencesAreEqual(keytipConfigMap.keytip1, keytip1Seq)).toEqual(true);
-    expect(fullKeySequencesAreEqual(keytipConfigMap.keytip2, keytip2Seq)).toEqual(true);
-    expect(fullKeySequencesAreEqual(keytipConfigMap.keytip3, keytip3Seq)).toEqual(true);
-    expect(fullKeySequencesAreEqual(keytipConfigMap.keytip4, keytip4Seq)).toEqual(true);
-    expect(fullKeySequencesAreEqual(keytipConfigMap.keytip5, keytip5Seq)).toEqual(true);
-    expect(fullKeySequencesAreEqual(keytipConfigMap.keytip6, keytip6Seq)).toEqual(true);
-    expect(fullKeySequencesAreEqual(keytipConfigMap.keytip7, keytip7Seq)).toEqual(true);
-    expect(fullKeySequencesAreEqual(keytipConfigMap.keytip8, keytip8Seq)).toEqual(true);
+    // Keytip1
+    let keytip1 = keytipConfigMap.keytip1;
+    expect(fullKeySequencesAreEqual(keytip1.keySequences, keytip1Seq)).toEqual(true);
+    expect(keytip1.content).toEqual('A');
+    expect(keytip1.onExecute).toBeDefined();
+
+    // Keytip2
+    let keytip2 = keytipConfigMap.keytip2;
+    expect(fullKeySequencesAreEqual(keytip2.keySequences, keytip2Seq)).toEqual(true);
+    expect(keytip2.content).toEqual('B1');
+    expect(keytip2.disabled).toEqual(true);
+
+    // Keytip3
+    let keytip3 = keytipConfigMap.keytip3;
+    expect(fullKeySequencesAreEqual(keytip3.keySequences, keytip3Seq)).toEqual(true);
+    expect(keytip3.content).toEqual('C');
+
+    // Keytip4
+    let keytip4 = keytipConfigMap.keytip4;
+    expect(fullKeySequencesAreEqual(keytip4.keySequences, keytip4Seq)).toEqual(true);
+    expect(keytip4.content).toEqual('T2');
+
+    // Keytip5
+    let keytip5 = keytipConfigMap.keytip5;
+    expect(fullKeySequencesAreEqual(keytip5.keySequences, keytip5Seq)).toEqual(true);
+    expect(keytip5.content).toEqual('F');
+
+    // Keytip6
+    let keytip6 = keytipConfigMap.keytip6;
+    expect(fullKeySequencesAreEqual(keytip6.keySequences, keytip6Seq)).toEqual(true);
+    expect(keytip6.content).toEqual('X0');
+    expect(keytip6.onReturn).toBeDefined();
+
+    // Keytip7
+    let keytip7 = keytipConfigMap.keytip7;
+    expect(fullKeySequencesAreEqual(keytip7.keySequences, keytip7Seq)).toEqual(true);
+    expect(keytip7.content).toEqual('YY');
+
+    // Keytip8
+    let keytip8 = keytipConfigMap.keytip8;
+    expect(fullKeySequencesAreEqual(keytip8.keySequences, keytip8Seq)).toEqual(true);
+    expect(keytip8.content).toEqual('R');
   });
 });
--- packages/experiments/src/utilities/keytip/KeytipConfig.ts
+++ packages/experiments/src/utilities/keytip/KeytipConfig.ts
@@ -1,37 +1,70 @@
 import { IKeySequence } from '../keysequence/IKeySequence';
 import { addKeytipSequence } from '../keytip/KeytipUtils';
+import { IKeytipProps } from '../../Keytip';
 
 export interface IKeytipConfig {
   keytips: IKeytipConfigItem[];
 }
 
 export interface IKeytipConfigItem {
+  /**
+   * Key Sequence for this keytip only
+   */
   sequence: IKeySequence;
+
+  /**
+   * Content for the keytip
+   */
+  content: string;
+
+  /**
+   * Identifier for the keytip, to be used to access in the configMap
+   */
   id: string;
+
+  /**
+   * Optional props in IKeytipProps
+   */
+  optionalProps?: Partial<IKeytipProps>;
+
+  /**
+   * Children keytips of this keytip
+   */
   children?: IKeytipConfigItem[];
 }
 
 export interface IKeytipConfigMap {
-  // TODO: should this point to an entire IKeytipProps?
-  [id: string]: IKeySequence[];
+  [id: string]: IKeytipProps;
 }
 
+/**
+ * Builds a map of ID -> IKeytipProps
+ *
+ * @param config - IKeytipConfig object
+ * @returns {IKeytipConfigMap} - Config map
+ */
 export function buildKeytipConfigMap(config: IKeytipConfig): IKeytipConfigMap {
   let configMap: IKeytipConfigMap = {};
 
   for (let keytip of config.keytips) {
-    constructKeytipSequence(configMap, [], keytip);
+    constructKeytip(configMap, [], keytip);
   }
 
   return configMap;
 }
 
-export function constructKeytipSequence(configMap: IKeytipConfigMap, parentSequence: IKeySequence[], keytip: IKeytipConfigItem): void {
+export function constructKeytip(configMap: IKeytipConfigMap, parentSequence: IKeySequence[], keytip: IKeytipConfigItem): void {
+  // Compute full key sequence
   let keytipSequence = addKeytipSequence(parentSequence, keytip.sequence);
-  configMap[keytip.id] = keytipSequence;
+
+  // Save props in configMap
+  let keytipProps: IKeytipProps = { ...keytip.optionalProps, keySequences: keytipSequence, content: keytip.content };
+  configMap[keytip.id] = keytipProps;
+
   if (keytip.children) {
     for (let child of keytip.children) {
-      constructKeytipSequence(configMap, keytipSequence, child);
+      // Create keytips for all children
+      constructKeytip(configMap, keytipSequence, child);
     }
   }
 }
